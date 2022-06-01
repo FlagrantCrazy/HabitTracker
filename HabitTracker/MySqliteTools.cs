1e381ff30255ad7ca8200e1056397d904da6ef7e
FlagrantCrazy <81555172+FlagrantCrazy@users.noreply.github.com>
2022-06-01 22:59:47 +1000

    commit 1

---
+++ HabitTracker/MySqliteTools.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.Data.Sqlite;
+
+public class MySqliteTools
+{
+
+	static string connectionString = @"Data Source=habit-tracker.db";
+
+	public void CreateDatabase()
+	{
+		/* Creates a Sqlite connection passing the connectionString
+		 * as an argument.  This will create the database. */
+		using (var connection = new SqliteConnection(connectionString))
+        {
+			// This is the command to send to the DB
+			using (var tableCmd = connection.CreateCommand())
+            {
+				connection.Open();
+
+				// The SQL syntax command is a property of tableCmd
+				tableCmd.CommandText =
+					@"CREATE TABLE IF NOT EXISTS yourHabit (
+						Id INTEGER PRIMARY KEY AUTOINCREMENT,
+						Date TEXT,
+						Quantity INTEGER
+						)";
+
+				// Executing the command, which is not a query,
+				// not fetching data
+				tableCmd.ExecuteNonQuery();
+            }
+        }
+	}
+}
