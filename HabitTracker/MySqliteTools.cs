75d1c31211463a045ae4b4270eafe994b4889b7b
FlagrantCrazy <81555172+FlagrantCrazy@users.noreply.github.com>
2022-06-03 00:05:24 +1000

    Added Sqlite db Create, Update and Delete methods

--- HabitTracker/MySqliteTools.cs
+++ HabitTracker/MySqliteTools.cs
@@ -9,37 +9,7 @@ public class MySqliteTools
 	public static void CreateDatabase()
 	{
 		/* Creates a Sqlite connection passing the connectionString
-		 * as an argument.  This will create the database. */
-		using (var connection = new SqliteConnection(connectionString))
-        {
-			// This is the command to send to the DB
-			using (var tableCmd = connection.CreateCommand())
-            {
-				connection.Open();
-
-				// The SQL syntax command is a property of tableCmd
-				tableCmd.CommandText =
-					@"CREATE TABLE IF NOT EXISTS CodingHours (
-						Id INTEGER PRIMARY KEY AUTOINCREMENT,
-						Date TEXT,
-						Quantity INTEGER
-						)";
-
-				// Executing the command, which is not a query,
-				// not fetching data
-				tableCmd.ExecuteNonQuery();
-            }
-        }
-		// Note 'using' closes the connection automatically like with ... as ...
-	}
-
-	public static void ReadDatabase()
-    {
-
-    }
-
-	public static void UpdateToDatabase(DateOnly date, int hoursCoded)
-    {
+			* as an argument.  This will create the database. */
 		using (var connection = new SqliteConnection(connectionString))
 		{
 			// This is the command to send to the DB
@@ -49,8 +19,33 @@ public class MySqliteTools
 
 				// The SQL syntax command is a property of tableCmd
 				tableCmd.CommandText =
-					@"INSERT INTO CodingHours (Date, Quantity)
-						VALUES (" + $"\"{date.ToString()}\", {hoursCoded});";
+					@"CREATE TABLE IF NOT EXISTS CodingHours (
+					Id INTEGER PRIMARY KEY AUTOINCREMENT,
+					Date TEXT,
+					Quantity INTEGER
+					)";
+
+				// Executing the command, which is not a query,
+				// not fetching data
+				tableCmd.ExecuteNonQuery();
+			}
+		}
+		// Note 'using' closes the connection automatically like with ... as ...
+	}
+
+	public static void CreateNewRecord(DateOnly date, int hoursCoded)
+	{
+		using (var connection = new SqliteConnection(connectionString))
+		{
+			// This is the command to send to the DB
+			using (var tableCmd = connection.CreateCommand())
+			{
+				connection.Open();
+
+				// The SQL syntax command is a property of tableCmd
+				tableCmd.CommandText =
+					$@"INSERT INTO CodingHours (Date, Quantity)
+					VALUES ('{date.ToString()}', {hoursCoded});";
 
 				// Executing the command, which is not a query,
 				// not fetching data
@@ -60,9 +55,78 @@ public class MySqliteTools
 		// Note 'using' closes the connection automatically like with ... 
 	}
 
-	public static void DeleteFromDatabase()
-    {
+	public static List<string[]> ReadAllRecords()
+	{
+		using (SqliteConnection connection = new SqliteConnection(connectionString))
+		{
+			// This is the command to send to the DB
+			using (SqliteCommand tableCmd = connection.CreateCommand())
+			{
+				connection.Open();
 
-    }
+				// The SQL syntax command is a property of tableCmd
+				tableCmd.CommandText =
+					@"SELECT * FROM CodingHours;";
 
+				// Executing the command and getting a data reader
+				SqliteDataReader reader = tableCmd.ExecuteReader();
+
+				// Initialise an empty List to store the records in
+				List<string[]> records = new List<string[]>();
+
+				// Advance through each record in the reader and add the record to the list
+				if (reader.HasRows)
+				{
+					while (reader.Read())
+					{
+						string[] record = new string[3] { reader.GetString(0), reader.GetString(1), reader.GetString(2) };
+						records.Add(record);
+					}
+				}
+				return records;
+			}
+		}
+	}
+
+	public static void UpdateExistingRecord(int id, DateOnly date, int hoursCoded)
+	{
+		using (SqliteConnection connection = new SqliteConnection(connectionString))
+		{
+			// This is the command to send to the DB
+			using (SqliteCommand tableCmd = connection.CreateCommand())
+			{
+				connection.Open();
+
+				// The SQL syntax command is a property of tableCmd
+				tableCmd.CommandText =
+					$@"UPDATE CodingHours
+					   SET Date = '{date.ToString()}', Quantity = {hoursCoded}
+					   WHERE Id = {id}";
+
+				// Executing the command and getting a data reader
+				tableCmd.ExecuteNonQuery();
+
+			}
+		}
+	} //quietly does nothing if target id doesn't exist
+
+	public static void DeleteRecord(int id)
+	{
+		using (SqliteConnection connection = new SqliteConnection(connectionString))
+		{
+			// This is the command to send to the DB
+			using (SqliteCommand tableCmd = connection.CreateCommand())
+			{
+				connection.Open();
+
+				// The SQL syntax command is a property of tableCmd
+				tableCmd.CommandText =
+					$@"DELETE FROM CodingHours WHERE Id = {id}";
+
+				// Executing the command and getting a data reader
+				tableCmd.ExecuteNonQuery();
+
+			}
+		}
+	}
 }
--- HabitTracker/Program.cs
+++ HabitTracker/Program.cs
@@ -1,14 +1,12 @@
-﻿namespace HabitTracker
+﻿using Microsoft.Data.Sqlite;
+
+namespace HabitTracker
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             MySqliteTools.CreateDatabase();
-
-            DateOnly mydate = new DateOnly(1994, 11, 2);
-
-            MySqliteTools.UpdateToDatabase(mydate, 1);
         }
     }
 }
